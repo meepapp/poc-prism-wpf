f98d1996d7284e07d87ea17ffbbbe9912ec12a0f
Breno Duarte <breno.duarte@meep-app.com>
2018-05-18 12:04:16 -0300

    Consertos

--- POC-PRISM-WPF/POC-PRISM-WPF/Views/ComprarWindowView.xaml.cs
+++ POC-PRISM-WPF/POC-PRISM-WPF/Views/ComprarWindowView.xaml.cs
@@ -24,5 +24,10 @@ namespace POC_PRISM_WPF.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
